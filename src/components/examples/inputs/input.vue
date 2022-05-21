be1d4284ab9b50b5018216ddfa133979347f2499
Jonathan Cruz <Jonathan.h.cruz@live.com>
2022-05-21 17:15:04 -0500

    tt-structure-styels

---
+++ src/components/examples/inputs/input.vue
@@ -0,0 +1,22 @@
+<template>
+  <input
+    :type="typeInput"
+    :placeholder="placeholderInput"
+    class="new-todo"
+    @:keyup.enter="eventInput"
+  />
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+export default class Input extends Vue {
+  valInput = "";
+
+  @Prop({ required: true, type: String }) readonly typeInput!: string;
+  @Prop({ required: true, type: String }) readonly placeholderInput!: string;
+  @Prop({ type: String }) readonly valueInput!: string;
+  @Prop({ type: Function }) eventInput!: (val: string) => void;
+}
+</script>
---
+++ src/components/organisms/TaskList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="toggle-all"></div>
+  <label for=""></label>
+
+  <div class="todo-list">
+    <ul>
+      <li class="editing">
+        <div class="view">
+          <input class="toggle" type="checkbox" />
+          <label>Buy milk</label>
+        </div>
+        <input class="edit" value="Buy milk" />
+
+        <!-- <div class="toggle"></div>
+            <label for="">hola</label> -->
+
+        <div class="destroy"></div>
+      </li>
+      <li>
+        <div class="view">
+          <input class="toggle" type="checkbox" />
+          <label>Buy milk</label>
+        </div>
+        <input class="edit" value="Buy milk" />
+
+        <div class="destroy"></div>
+      </li>
+      <li class="completed">
+        <div class="view">
+          <input class="toggle" type="checkbox" />
+          <label>Buy milk</label>
+        </div>
+        <input class="edit" value="Buy milk" />
+
+        <div class="destroy"></div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+// import { Ref } from "vue-property-decorator";
+
+// import ToggleAll from "@/components/organisms/TaskList.vue";
+
+// // @Options({
+// //   components: {
+// //     ToggleAll,
+// //   },
+// // })
+export default class TaskList extends Vue {
+  //   @Ref() toggleAll!: ToggleAll;
+}
+</script>
