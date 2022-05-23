5a9c844fd8e093e21475c110b3959be94247f7ee
Jonathan Cruz <Jonathan.h.cruz@live.com>
2022-05-23 14:45:11 -0500

    tt-remove-task-examples

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,24 +1,5 @@
 export default {
   isEditing: false,
   changesExist: false,
-  tasks: [
-    {
-      id: "77a9sdf7a0ds76g6a32452",
-      description: "Task 1 description",
-      completed: false,
-      statusTask: "read",
-    },
-    {
-      id: "asdg7sdg7as8dg770987fa",
-      description: "Task 2 description",
-      completed: false,
-      statusTask: "read",
-    },
-    {
-      id: "safg98077a0sdg0ga9sd7g",
-      description: "Task 3 description",
-      completed: false,
-      statusTask: "read",
-    },
-  ],
+  tasks: [],
 };
