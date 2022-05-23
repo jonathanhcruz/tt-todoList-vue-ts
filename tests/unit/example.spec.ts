94f5a00dc5911929474ac61aa286f1e1efb3b40a
Jonathan Cruz <Jonathan.h.cruz@live.com>
2022-05-23 14:49:20 -0500

    tt-remove-componentes-examples

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,5 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/examples/HelloWorld.vue";
+// import { shallowMount } from "@vue/test-utils";
 
 describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+  it("renders props.msg when passed", () => {});
 });
